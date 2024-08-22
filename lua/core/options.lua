beee328eea840e0f00fbf251fe063952be567d50
ThoriumSnake <thoriumsnake@outlook.com>
2024-08-21 21:41:54 -0300

    Change tab formatting to use 4 spaces

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -9,3 +9,8 @@ vim.cmd("colorscheme cyberdream")
 vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#A05C7B', bold=true })
 vim.api.nvim_set_hl(0, 'LineNr', { fg='white', bold=true })
 vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='#A05C7B', bold=true })
+
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
