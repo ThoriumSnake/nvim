474f19596a39403238eb68586cacae31f4391047
ThoriumSnake <thoriumsnake@outlook.com>
2024-08-21 21:47:08 -0300

    Tweak some remaps

--- lua/core/remap.lua
+++ lua/core/remap.lua
@@ -3,10 +3,13 @@ vim.keymap.set("", "ñ", " ", { remap = true })
 
 vim.keymap.set("n", "<leader>p", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
-vim.keymap.set("n", "<leader>q", "<CMD>bd<CR>", { desc = "Close buffer" })
+vim.keymap.set("n", "<leader>q", "<CMD>q<CR>", { desc = "Quit buffer" })
 vim.keymap.set("n", "<leader>qa", "<CMD>qa<CR>", { desc = "Close all buffers" })
 
 vim.keymap.set("", "<leader>w", "<CMD>w<CR>", { desc = "Write buffer" })
 vim.keymap.set("", "<C-S>", "<CMD>w<CR>", { desc = "Write buffer" })
 
 vim.keymap.set("n", "<C-A>", "ggVG$", { desc = "Select all" })
+
+vim.keymap.set("n", "U", "<C-r>", { desc = "Redo" })
+
