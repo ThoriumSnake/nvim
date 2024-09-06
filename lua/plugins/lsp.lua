a6623518f9b165fa7971909263220dfbbbb2c415
ThoriumSnake <thoriumsnake@outlook.com>
2024-09-06 19:27:51 -0300

    Fix LSP and Treesitter not highlighting in JS/Vue

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -60,6 +60,27 @@ return {
                     }
                 end,
 
+                tsserver = function()
+                local mason_registry = require('mason-registry')
+                    local vue_language_server_path = mason_registry.get_package('vue-language-server'):get_install_path() .. '/node_modules/@vue/language-server'
+
+                    local lspconfig = require("lspconfig")
+                    lspconfig.tsserver.setup {
+                        init_options = {
+                            plugins = {
+                                {
+                                    name = '@vue/typescript-plugin',
+                                    location = vue_language_server_path,
+                                    languages = { 'vue' },
+                                },
+                            },
+                        },
+                        filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+                    }
+                    -- No need to set `hybridMode` to `true` as it's the default value
+                    lspconfig.volar.setup {}
+                end,
+
                 zls = function()
                     local lspconfig = require("lspconfig")
                     lspconfig.zls.setup({
