b8836454701ef3032f6d15af879541af09f2d31a
ThoriumSnake <thoriumsnake@outlook.com>
2024-09-06 19:17:24 -0300

    Add settings for cmd and statusline visuals

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -10,6 +10,8 @@ vim.opt.linebreak = true
 vim.opt.breakindent = true
 vim.opt.scrolloff = 9
 
+vim.go.laststatus = 3
+
 vim.cmd("colorscheme cyberdream")
 
 vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#A05C7B', bold=true })
@@ -20,3 +22,5 @@ vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
+vim.opt.cmdheight = 0
