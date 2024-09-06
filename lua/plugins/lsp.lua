4b8d3ec5f571c70d4c49fb6dbdd86bd7eb79749d
ThoriumSnake <thoriumsnake@outlook.com>
2024-09-06 19:26:50 -0300

    Lsp mapping configuration

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -13,6 +13,33 @@ return {
     },
 
     config = function()
+        -- note: diagnostics are not exclusive to lsp servers
+        -- so these can be global keybindings
+        vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
+        vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
+        vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
+
+        vim.api.nvim_create_autocmd('LspAttach', {
+            desc = 'LSP actions',
+            callback = function(event)
+                local opts = {buffer = event.buf}
+
+                -- these will be buffer-local keybindings
+                -- because they only work if you have an active language server
+
+                vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+                vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+                vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+                vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+                vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+                vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+                vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+                vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+                vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+                vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+            end
+        })
+
         local cmp = require('cmp')
         local cmp_lsp = require("cmp_nvim_lsp")
         local capabilities = vim.tbl_deep_extend(
@@ -49,6 +76,7 @@ return {
                     vim.g.zig_fmt_autosave = 0
 
                 end,
+
                 ["lua_ls"] = function()
                     local lspconfig = require("lspconfig")
                     lspconfig.lua_ls.setup {
@@ -63,10 +91,9 @@ return {
                         }
                     }
                 end,
+
             }
         })
-
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
         cmp.setup({
             snippet = {
@@ -75,16 +102,29 @@ return {
                 end,
             },
 
+            preselect = 'item',
+            completion = {
+                autocomplete = false,
+                completeopt = 'menu,menuone,noinsert',
+            },
+
             mapping = cmp.mapping.preset.insert({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<CR>'] = cmp.mapping.confirm({ select = true }),
-                ['<Space>'] = cmp.mapping.confirm({ select = false }),
+                ['k'] = cmp.mapping.select_prev_item({behavior = cmp.SelectBehavior.Select }),
+                ['j'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+                ['<CR>'] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace }),
+                ['<Space>'] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace }),
                 ['<C-Space>'] = cmp.mapping.complete(),
                 ['<C-u>'] = cmp.mapping.scroll_docs(-4),
                 ['<C-d>'] = cmp.mapping.scroll_docs(4),
+
+                --To close menu without exiting insert
+                --['<Esc>'] = cmp.mapping(function(fallback)
+                --    if cmp.visible() then
+                --        cmp.close()
+                --    else
+                --        fallback()  -- This will pass the key to Neovim if the completion menu is not visible.
+                --    end
+                --end, { 'i', 'c' }),
             }),
 
             sources = cmp.config.sources({
