79026df53771fb218c15a1c4ca4083f88aae6e06
ThoriumSnake <thoriumsnake@outlook.com>
2024-12-27 21:56:43 -0300

    Format this file I guess??

--- lua/core/highlights.lua
+++ lua/core/highlights.lua
@@ -1,21 +1,21 @@
 local color = require("utils.colors")
 
-local background = vim.api.nvim_get_hl(0, {name = "Normal"}).bg
-local error = vim.api.nvim_get_hl(0, {name = "DiagnosticError"}).fg
-local warn = vim.api.nvim_get_hl(0, {name = "DiagnosticWarn"}).fg
-local info = vim.api.nvim_get_hl(0, {name = "DiagnosticInfo"}).fg
-local hint = vim.api.nvim_get_hl(0, {name = "DiagnosticHint"}).fg
+local background = vim.api.nvim_get_hl(0, { name = "Normal" }).bg
+local error = vim.api.nvim_get_hl(0, { name = "DiagnosticError" }).fg
+local warn = vim.api.nvim_get_hl(0, { name = "DiagnosticWarn" }).fg
+local info = vim.api.nvim_get_hl(0, { name = "DiagnosticInfo" }).fg
+local hint = vim.api.nvim_get_hl(0, { name = "DiagnosticHint" }).fg
 
 local blend_error = color.blend(error, background, 0.15)
 local blend_warn = color.blend(warn, background, 0.15)
 local blend_info = color.blend(info, background, 0.15)
 local blend_hint = color.blend(hint, background, 0.15)
 
+--NOTE: Don't use the blend parameter, it blends the window with the desktop lmao
 vim.api.nvim_set_hl(0, "DiagnosticErrorLn", { bg = blend_error })
 vim.api.nvim_set_hl(0, "DiagnosticWarnLn", { bg = blend_warn })
 vim.api.nvim_set_hl(0, "DiagnosticInfoLn", { bg = blend_info })
 vim.api.nvim_set_hl(0, "DiagnosticHintLn", { bg = blend_hint })
-
 
 -- local indent = vim.api.nvim_get_hl(0, {name = "MiniIndentscopeSymbol"}).fg
 -- vim.api.nvim_set_hl(0, "MiniIndentscopeSymbol", { fg = indent == "#ffffff" and "#00a5c5a" or indent })
--- lua/core/mode-recolor.lua
+++ lua/core/mode-recolor.lua
@@ -1,11 +1,9 @@
-
 local color = require("utils.colors")
 
-local initial = vim.api.nvim_get_hl(0, {name = "CursorLine"}).bg
-local background = vim.api.nvim_get_hl(0, {name = "Normal"}).bg
-local lualine_hl = vim.api.nvim_get_hl(0, {name = "lualine_a_insert"})
-local insert = lualine_hl.bg == vim.api.nvim_get_hl(0, {name = "Normal"}).bg and lualine_hl.fg or lualine_hl.bg
-
+local initial = vim.api.nvim_get_hl(0, { name = "CursorLine" }).bg
+local background = vim.api.nvim_get_hl(0, { name = "Normal" }).bg
+local lualine_hl = vim.api.nvim_get_hl(0, { name = "lualine_a_insert" })
+local insert = lualine_hl.bg == vim.api.nvim_get_hl(0, { name = "Normal" }).bg and lualine_hl.fg or lualine_hl.bg
 
 local blend_insert = color.blend(insert, background, 0.15)
 
@@ -23,20 +21,20 @@ local blend_insert = color.blend(insert, background, 0.15)
 
 vim.api.nvim_create_autocmd({ "InsertEnter" }, {
     callback = function()
-        vim.api.nvim_set_hl(0, "CursorLine", {bg=blend_insert})
-    end
+        vim.api.nvim_set_hl(0, "CursorLine", { bg = blend_insert })
+    end,
 })
 
 vim.api.nvim_create_autocmd({ "InsertLeave" }, {
     callback = function()
-        vim.api.nvim_set_hl(0, "CursorLine", {bg=initial})
-    end
+        vim.api.nvim_set_hl(0, "CursorLine", { bg = initial })
+    end,
 })
 
 vim.api.nvim_create_autocmd("ModeChanged", {
-  -- pattern = "*:[vV\xV]",
-  pattern = "*:n",
-  callback = function()
-      vim.api.nvim_set_hl(0, "CursorLine", {bg=initial})
-  end,
+    -- pattern = "*:[vV\xV]",
+    pattern = "*:n",
+    callback = function()
+        vim.api.nvim_set_hl(0, "CursorLine", { bg = initial })
+    end,
 })
