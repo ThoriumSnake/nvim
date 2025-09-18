b45eeae67b9ee1de4b64c0cfe9ccb8bc2ced8860
ThoriumSnake <thoriumsnake@outlook.com>
2025-09-17 21:45:06 -0300

    A lot of fuckery I will have to destroy

--- lua/lsp/lspconfig.lua
+++ lua/lsp/lspconfig.lua
@@ -49,6 +49,11 @@ return {
                 prefix = " 󰊠",
             },
 
+            -- virtual_lines = {
+            --     prefix = " 󰊠",
+            --     current_line = false,
+            -- },
+
             float = {
                 focusable = false,
                 source = "always",
@@ -82,6 +87,7 @@ return {
         ---- LANGUAGE SETUP ----
         require("mason-lspconfig").setup({
             ensure_installed = { "lua_ls" },
+            -- automatic_enable = false,
             handlers = {
                 -- this first function is the "default handler"
                 -- it applies to every language server without a "custom handler"
@@ -89,29 +95,29 @@ return {
                     require("lspconfig")[server_name].setup({})
                 end,
 
-                ts_ls = function()
-                    local mason_registry = require("mason-registry")
-                    local vue_language_server_path = mason_registry
-                        .get_package("vue-language-server")
-                        :get_install_path() .. "/node_modules/@vue/language-server"
-
-                    local lspconfig = require("lspconfig")
-                    lspconfig.ts_ls.setup({
-                        init_options = {
-                            plugins = {
-                                {
-                                    name = "@vue/typescript-plugin",
-                                    location = vue_language_server_path,
-                                    -- languages = { "javascript", "typescript", "vue" },
-                                    languages = { "vue" },
-                                },
-                            },
-                        },
-                        filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
-                    })
-                    -- No need to set `hybridMode` to `true` as it's the default value
-                    -- lspconfig.volar.setup({})
-                end,
+                -- ts_ls = function()
+                --     local mason_registry = require("mason-registry")
+                --     local vue_language_server_path = mason_registry
+                --         .get_package("vue-language-server")
+                --         :get_install_path() .. "/node_modules/@vue/language-server"
+                --
+                --     local lspconfig = require("lspconfig")
+                --     lspconfig.ts_ls.setup({
+                --         init_options = {
+                --             plugins = {
+                --                 {
+                --                     name = "@vue/typescript-plugin",
+                --                     location = vue_language_server_path,
+                --                     -- languages = { "javascript", "typescript", "vue" },
+                --                     languages = { "vue" },
+                --                 },
+                --             },
+                --         },
+                --         filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+                --     })
+                --     -- No need to set `hybridMode` to `true` as it's the default value
+                --     -- lspconfig.volar.setup({})
+                -- end,
 
                 omnisharp_mono = function()
                     local pid = vim.fn.getpid()
@@ -142,11 +148,73 @@ return {
         --         return vim.fs.dirname(vim.fs.find("project.godot", { upward = true })[1])
         --     end,
         -- })
-        require("lspconfig").gdscript.setup({
+
+        local lspconfig = require("lspconfig")
+        lspconfig.gdscript.setup({
             -- cmd = { "ncat", "localhost", "6005" },
             -- root_dir = function()
             --     return vim.fs.dirname(vim.fs.find("project.godot", { upward = true })[1])
             -- end,
         })
+
+        local vue_language_server_path = vim.fn.expand("$MASON/packages")
+            .. "/vue-language-server"
+            .. "/node_modules/@vue/language-server"
+        local vue_plugin = {
+            name = "@vue/typescript-plugin",
+            location = vue_language_server_path,
+            languages = { "vue" },
+            configNamespace = "typescript",
+        }
+        local vtsls_config = {
+            settings = {
+                vtsls = {
+                    tsserver = {
+                        globalPlugins = {
+                            vue_plugin,
+                        },
+                    },
+                },
+            },
+            filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+        }
+
+        local vue_ls_config = {
+            settings = {
+                filetypes = { "vue" },
+            },
+            on_init = function(client)
+                client.handlers["tsserver/request"] = function(_, result, context)
+                    local clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = "vtsls" })
+                    if #clients == 0 then
+                        vim.notify(
+                            "Could not find `vtsls` lsp client, `vue_ls` would not work without it.",
+                            vim.log.levels.ERROR
+                        )
+                        return
+                    end
+                    local ts_client = clients[1]
+
+                    local param = unpack(result)
+                    local id, command, payload = unpack(param)
+                    ts_client:exec_cmd({
+                        title = "vue_request_forward", -- You can give title anything as it's used to represent a command in the UI, `:h Client:exec_cmd`
+                        command = "typescript.tsserverRequest",
+                        arguments = {
+                            command,
+                            payload,
+                        },
+                    }, { bufnr = context.bufnr }, function(_, r)
+                        local response_data = { { id, r.body } }
+                        ---@diagnostic disable-next-line: param-type-mismatch
+                        client:notify("tsserver/response", response_data)
+                    end)
+                end
+            end,
+        }
+        -- -- nvim 0.11 or above
+        vim.lsp.config("vtsls", vtsls_config)
+        vim.lsp.config("vue_ls", vue_ls_config)
+        vim.lsp.enable({ "vtsls", "vue_ls" })
     end,
 }
